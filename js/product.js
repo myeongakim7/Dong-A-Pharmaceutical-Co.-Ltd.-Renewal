a92ab6aefd87aa8ea991706c70c550a2c399f733
myeongakim7 <113976428+myeongakim7@users.noreply.github.com>
2022-12-09 03:50:08 +0900

    sub) 제품 정보 - 항목 선택 시 항목 분류
    제품의 항목을 각 각 선택하여 해당 항목만 볼 수 있게 구현함

---
+++ js/product.js
@@ -0,0 +1,54 @@
+// 사이트맵 버튼
+
+$(function () {
+  $(".m_btn a").on("click", function () {
+    $(".site_map").toggleClass("site_map_open");
+  });
+
+  $(".site_map_close_btn a").on("click", function () {
+    $(".site_map").removeClass("site_map_open");
+  });
+});
+
+// 제품 검색
+
+// product.js
+
+$(function () {
+  $(".depth2 .depth2_ul .depth2_li a")
+    .eq(0)
+    .on("click", function () {
+      $(".pro_ul .pro_li-2, .pro_li-3, .pro_li-4").addClass("none"),
+        $(".pro_ul .pro_li-1").addClass("show");
+      $(".pro_ul .pro_li-2").removeClass("show");
+    });
+
+  $(".depth2 .depth2_ul .depth2_li a")
+    .eq(1)
+    .on("click", function () {
+      $(".pro_ul .pro_li-1, .pro_li-3, .pro_li-4").addClass("none"),
+        $(".pro_ul .pro_li-2").addClass("show");
+      $(".pro_ul .pro_li-1").removeClass("show");
+      $(".pro_ul .pro_li-3").removeClass("show");
+    });
+
+  $(".depth2 .depth2_ul .depth2_li a")
+    .eq(2)
+    .on("click", function () {
+      $(".pro_ul .pro_li-1, .pro_li-2, .pro_li-4").addClass("none"),
+        $(".pro_ul .pro_li-3").addClass("show");
+      $(".pro_ul .pro_li-1").removeClass("show");
+      $(".pro_ul .pro_li-2").removeClass("show");
+      $(".pro_ul .pro_li-4").removeClass("show");
+    });
+
+  $(".depth2 .depth2_ul .depth2_li a")
+    .eq(3)
+    .on("click", function () {
+      $(".pro_ul .pro_li-1, .pro_li-2, .pro_li-3").addClass("none"),
+        $(".pro_ul .pro_li-4").addClass("show");
+      $(".pro_ul .pro_li-1").removeClass("show");
+      $(".pro_ul .pro_li-2").removeClass("show");
+      $(".pro_ul .pro_li-3").removeClass("show");
+    });
+});
