6ce1e91275433749d82f12f59da998c7b4a6d86c
myeongakim7 <113976428+myeongakim7@users.noreply.github.com>
2023-03-07 02:39:32 +0900

    main 풀 스크린 화면 구현 / 구글 번역 API 추가
    -main 풀 스크린 화면 구현
    -구글 번역 API 추가
    -헤더 부분 반응형 오류 수정

--- main/js/main.js
+++ main/js/main.js
@@ -9,17 +9,32 @@ $(function () {
     "main/images/main-visual3.jpg",
   ];
 
-  let bnTitle_1 = ["언제나 함께", "Since 1963", "비타그란 비타민C"];
+  let bnTitle_1 = [
+    "Just to make you happy smile",
+    "Since 1963",
+    "비타그란 비타민C",
+  ];
   let bnTitle_2 = [
-    "건강한 생활 속에서<br>동아제약이 함께합니다",
-    "당신의 피로회복제 박카스 <br> 다양한 박카스 제품을 비교해보세요!",
-    "#비타민C #취향맞춤비타민C <br> 언제 어디서나 비타민C의 생기를 가득 만나보세요",
+    "That's the our motive",
+    "당신의 피로회복제 박카스",
+    "본연의 에너지에 충실한 비타민C의 기준",
   ];
   let bnTitle_3 = [
-    "<a href='https://dmall.co.kr/product/index.html' style='color:var(--sub)'>공식몰 바로가기</a>",
-    "<a href='http://www.bacchusd.com/' style='color:#101A39'>박카스 바로가기</a>",
-    "<a href='https://dmall.co.kr/product/list.html?cate_no=359' style='color:#CE2424'>비타그란 바로가기</a>",
+    "더 나은 삶에 대한 믿음, 하루를 새롭게 시작할 수 있는 에너지<br>동아제약이 함께 하겠습니다.",
+    "오늘 하루도 힘을 냈던 당신,<br>스스로에게 작은 칭찬과 '박카스 한병' 선물하세요",
+    "언제 어디서나 비타민C의 생기를 가득 느껴보세요<br> #비타민C #필수영양소 #취향맞춤비타민C ",
   ];
+  let bnTitle_4 = [
+    "<a href='https://dmall.co.kr/product/index.html'>공식몰 바로가기&nbsp;→</a>",
+    "<a href='http://www.bacchusd.com/'>박카스 바로가기&nbsp;→</a>",
+    "<a href='https://dmall.co.kr/product/list.html?cate_no=359'>비타그란 바로가기&nbsp;→</a>",
+  ];
+
+  // let bnTitle_4 = [
+  //   "<a href='https://dmall.co.kr/product/index.html' style='color:var(--sub)'>공식몰 바로가기</a>",
+  //   "<a href='http://www.bacchusd.com/' style='color:#101A39'>박카스 바로가기</a>",
+  //   "<a href='https://dmall.co.kr/product/list.html?cate_no=359' style='color:#CE2424'>비타그란 바로가기</a>",
+  // ];
 
   let bg_img_num = 0;
 
@@ -29,20 +44,49 @@ $(function () {
       bg_img_num = 0;
     }
 
-    $(".main_bn").css({
+    $("#header-banner-wrap").css({
       "background-image": `url(${bgArr[bg_img_num]})`,
     });
 
-    $(".main_bn").css({
+    $("#header-banner-wrap").css({
       transition: "1s",
       opacity: "1",
     });
 
     $(".main_bn_title h2").html(bnTitle_1[bg_img_num]);
     $(".main_bn_title h3").html(bnTitle_2[bg_img_num]);
-    $(".main_bn_title p").html(bnTitle_3[bg_img_num]);
+    $(".main_bn_title h4").html(bnTitle_3[bg_img_num]);
+    $(".main_bn_title p").html(bnTitle_4[bg_img_num]);
   }, 6000);
 });
+
+// hover 시 로고, 아이콘 이미지 변경
+$(function () {
+  $(".header").mouseover(function () {
+    $(".logo a img").attr("src", "./main/images/main-logo.svg");
+    $(".img_lang img").attr("src", "./main/images/icon-lang.svg");
+    $(".m_btn a img").attr("src", "/main/images/icon-menu.svg");
+  });
+  $(".header").mouseout(function () {
+    $(".logo a img").attr("src", "./main/images/main-logo-white.svg");
+    $(".img_lang img").attr("src", "./main/images/icon-lang-white.svg");
+    $(".m_btn a img").attr("src", "/main/images/icon-menu-white.svg");
+  });
+});
+
+// 구글 번역 스크립트
+
+// function googleTranslateElementInit() {
+//   new google.translate.TranslateElement(
+//     {
+//       pageLanguage: "ko",
+//       includedLanguages: "ko,zh-CN,zh-TW,ja,vi,th,tl,km,my,mn,ru,en,fr,ar",
+//       layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+//       autoDisplay: false,
+//     },
+//     "google_translate_element"
+//   );
+// }
 
 // 사이트맵 버튼
 
