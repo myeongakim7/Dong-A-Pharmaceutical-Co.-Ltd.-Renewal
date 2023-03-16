1165f42e737d2c607c03f12222b741fb1f29231e
myeongakim7 <113976428+myeongakim7@users.noreply.github.com>
2023-03-17 00:02:33 +0900

    서브페이지) 제품 소개 - 페이지 넘기기 구현 (pc/mobile)

--- product/js/product.js
+++ product/js/product.js
@@ -17,8 +17,8 @@ if (screen.width > 768) {
     $(".depth2 .depth2_ul .depth2_li").eq(0).on("click", normal);
     function normal() {
       $(".product-list .pro_ul").css({
-        top: 0,
-        height: "-100%",
+        position: "absolute",
+        top: "100px",
       });
       $(".product-list .container").css({ height: "1500px" });
       $(".product-list .pro_ul .pro-li").css({ display: "none" });
@@ -55,6 +55,54 @@ if (screen.width > 768) {
       $(".product-list .pro_ul .pro_li-4").css({ display: "block" });
     }
   });
+
+  // 다이얼
+  $(function () {
+    $(".dial .numbers p")
+      .eq(0)
+      .on("click", function () {
+        $(".product-list .container .pro_ul").css({
+          position: "absolute",
+          top: "100px",
+        });
+      });
+    $(".dial .numbers p")
+      .eq(1)
+      .on("click", function () {
+        $(".product-list .container .pro_ul").css({
+          position: "absolute",
+          top: "-88%",
+        });
+      });
+    // leftMax
+    $(".dial .numbers .leftMax").on("click", function () {
+      $(".product-list .container .pro_ul").css({
+        position: "absolute",
+        top: "100px",
+      });
+    });
+    // left
+    $(".dial .numbers .left").on("click", function () {
+      $(".product-list .container .pro_ul").css({
+        position: "absolute",
+        top: "100px",
+      });
+    });
+    // rightMax
+    $(".dial .numbers .rightMax").on("click", function () {
+      $(".product-list .container .pro_ul").css({
+        position: "absolute",
+        top: "-88%",
+      });
+    });
+    // right
+    $(".dial .numbers .right").on("click", function () {
+      $(".product-list .container .pro_ul").css({
+        position: "absolute",
+        top: "-88%",
+      });
+    });
+  });
 }
 
 // 모바일 버전
@@ -66,10 +114,10 @@ if (screen.width < 769) {
     $(".depth2 .depth2_ul .depth2_li").eq(0).on("click", normal);
     function normal() {
       $(".product-list .pro_ul").css({
-        top: 0,
-        height: "-100%",
+        position: "absolute",
+        top: "50px",
       });
-      $(".product-list .container").css({ height: "1500px" });
+      // $(".product-list .container").css({ height: "500px" });
       $(".product-list .pro_ul .pro-li").css({ display: "none" });
       $(".product-list .pro_ul .pro_li-2").css({ display: "none" });
       $(".product-list .pro_ul .pro_li-1").css({ display: "block" });
@@ -77,7 +125,7 @@ if (screen.width < 769) {
 
     $(".depth2 .depth2_ul .depth2_li").eq(1).on("click", except);
     function except() {
-      $(".product-list .container").css({ height: "1000px" });
+      // $(".product-list .container").css({ height: "1000px" });
       $(".product-list .pro-ul .pro-li").css({ display: "none" });
       $(".product-list .pro_ul .pro_li-1").css({ display: "none" });
       $(".product-list .pro_ul .pro_li-3").css({ display: "none" });
@@ -87,7 +135,7 @@ if (screen.width < 769) {
 
     $(".depth2 .depth2_ul .depth2_li").eq(2).on("click", food);
     function food() {
-      $(".product-list .container").css({ height: "1000px" });
+      // $(".product-list .container").css({ height: "1000px" });
       $(".product-list .pro-ul  .pro-li").css({ display: "none" });
       $(".product-list .pro_ul .pro_li-1").css({ display: "none" });
       $(".product-list .pro_ul .pro_li-2").css({ display: "none" });
@@ -96,12 +144,59 @@ if (screen.width < 769) {
     }
     $(".depth2 .depth2_ul .depth2_li").eq(3).on("click", life);
     function life() {
-      $(".product-list .container").css({ height: "1000px" });
+      // $(".product-list .container").css({ height: "1000px" });
       $(".product-list .pro-ul .pro-li").css({ display: "none" });
       $(".product-list .pro_ul .pro_li-1").css({ display: "none" });
       $(".product-list .pro_ul .pro_li-2").css({ display: "none" });
       $(".product-list .pro_ul .pro_li-3").css({ display: "none" });
       $(".product-list .pro_ul .pro_li-4").css({ display: "block" });
     }
+  });
+
+  // 다이얼
+  $(function () {
+    $(".dial .numbers p")
+      .eq(0)
+      .on("click", function () {
+        $(".product-list .container .pro_ul").css({
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+        });
+      });
+    $(".dial .numbers p")
+      .eq(1)
+      .on("click", function () {
+        $(".product-list .container .pro_ul").css({
+          position: "absolute",
+          top: "-88%",
+        });
+      });
+    // leftMax
+    $(".dial .numbers .leftMax").on("click", function () {
+      $(".product-list .container .pro_ul").css({
+        position: "absolute",
+      });
+    });
+    // left
+    $(".dial .numbers .left").on("click", function () {
+      $(".product-list .container .pro_ul").css({
+        position: "absolute",
+      });
+    });
+    // rightMax
+    $(".dial .numbers .rightMax").on("click", function () {
+      $(".product-list .container .pro_ul").css({
+        position: "absolute",
+        top: "-88%",
+      });
+    });
+    // right
+    $(".dial .numbers .right").on("click", function () {
+      $(".product-list .container .pro_ul").css({
+        position: "absolute",
+        top: "-88%",
+      });
+    });
   });
 }
